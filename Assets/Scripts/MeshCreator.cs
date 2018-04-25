91f28a9227849f9d22be7bfa0d5172677d57da64
James Morrow <jamesmorrowdesign@gmail.com>
2018-04-24 22:36:30 -0700

    Mesh Terrain.

---
+++ Assets/Scripts/MeshCreator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class MeshCreator
+{
+    private List<Vector3> verts = new List<Vector3>();
+    private List<Vector3> normals = new List<Vector3>();
+    private List<Vector2> uvs = new List<Vector2>();
+    private List<Color> colors = new List<Color>();
+    private List<int> triangleIndices = new List<int>();
+    private Mesh mesh;
+    public int vertexCount { get { return verts.Count; } }
+
+    public Mesh CreateMesh()
+    {
+        mesh = new Mesh();
+        mesh.vertices = verts.ToArray();
+        mesh.normals = normals.ToArray();
+        mesh.uv = uvs.ToArray();
+        mesh.triangles = triangleIndices.ToArray();
+        mesh.colors = colors.ToArray();
+        return mesh;
+    }
+
+    public void BuildTriangle(int i0, int i1, int i2)
+    {
+        triangleIndices.Add(i0);
+        triangleIndices.Add(i1);
+        triangleIndices.Add(i2);
+    }
+
+    public void AddVertex(Vector3 v, Vector3 normal, Vector2 uv, Color color)
+    {
+        verts.Add(v);
+        normals.Add(normal);
+        uvs.Add(uv);
+        colors.Add(color);
+    }
+
+    public void Clear()
+    {
+        if (mesh != null)
+        {
+            mesh.Clear();
+        }
+        verts.Clear();
+        normals.Clear();
+        uvs.Clear();
+        triangleIndices.Clear();
+        colors.Clear();
+    }
+}
---
+++ Assets/Scripts/ProceduralTerrain.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+
+public class ProceduralTerrain : MonoBehaviour 
+{
+    public Vector3 noiseScale; 
+    [Range(0, 255)] public int width;
+    [Range(0, 255)] public int depth;
+    public float maxHeight;
+    public float squareSize;
+    private MeshCreator mc;
+    private MeshFilter meshFilter;
+
+	void Start () 
+	{
+        mc = new MeshCreator();
+        meshFilter = GetComponent<MeshFilter>();
+	}
+	
+	void Update () 
+	{
+        mc.Clear();
+
+        // add vertices
+        for (int i=0; i<=width; i++)
+        {
+            float x = i * squareSize;
+            for (int j=0; j<=depth; j++)
+            {
+                float z = j * squareSize;
+
+                // compute y-value using noise
+                float noiseOut = Perlin.Noise(noiseScale.x * i, noiseScale.y * Time.time, noiseScale.z * j);
+                noiseOut += 1;
+                noiseOut /= 2;
+                float y = noiseOut * maxHeight;
+
+                // compute uv
+                float u = (float)i / width;
+                float v = (float)j / depth;
+
+                Debug.Log(u + ", " + v);
+
+                // add the vertex
+                Color color = Random.value < 0.5 ? Color.blue : Color.green;
+                mc.AddVertex(new Vector3(x, y, z), Vector3.up, new Vector2(u, v), color);
+            }
+        }
+
+        // build triangles out of those vertices
+        {
+            int i = 0;
+            while (i + depth + 1 < mc.vertexCount)
+            {
+                mc.BuildTriangle(i, i + depth, i + depth + 1);
+                mc.BuildTriangle(i, i + depth + 1, i + 1);
+                i++;
+            }
+        }
+
+        meshFilter.mesh = mc.CreateMesh();
+	}
+}
